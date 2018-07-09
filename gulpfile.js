19c38f6502c158f6cd88ce3bcb58f13eef3714c0
Fey Ijaware <fshola4real@yahoo.co.uk>
2018-07-09 17:28:33 +0100

    reshuffled files and added in my google api key from google cloud platform

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
 
-gulp.task('sass', function(){ 
-return gulp.src('scss/styles.scss') 
-.pipe(sass()) // Converts Sass to CSS with gulp-sass 
-.pipe(gulp.dest('css')) });
+gulp.task('sass', function(){
+  //return gulp.src('app/scss/styles.scss') // this only lets us compile .scss file
+  // using globbing allow us to compile more than one
+  return gulp.src('app/scss/**/*.scss') // using Globbing gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('app/css'))
+});
