7c7a778f990c42d0228ac1a437de4289e0fa6bf7
Fey Ijaware <fshola4real@yahoo.co.uk>
2018-07-10 07:18:58 +0100

    created an index.js file for the service worker registration and included the file extension into the html files

---
+++ js/index.js
@@ -0,0 +1,12 @@
+if ('serviceWorker' in navigator) {
+            window.addEventListener('load', function() {
+                navigator.serviceWorker.register('sw.js').then(function(registration) {
+                    // registration success
+                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                }, function(err) {
+                    // registration failure
+                    console.log('ServiceWorker registration failed: ', err);
+                });
+            });
+        }
+    
