39fc1ee4a37ee39f45a598b04d4efcebae340042
Fey Ijaware <fshola4real@yahoo.co.uk>
2018-07-10 05:27:38 +0100

    added in sw.js for caching

---
+++ sw.js
@@ -0,0 +1,35 @@
+var staticCacheName = 'restaurant-v1.0';
+var staticImageCacheName = 'restaurant-imgs';
+var allCaches = [
+    staticCacheName,
+    staticImageCacheName
+];
+var urlsToCache = [
+    '/',
+    '/css/styles.css',
+    '/index.html',
+    '/restaurant.html',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js'
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                return response || fetch(event.request).then(function (response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                })
+            })
+        }))
+});
