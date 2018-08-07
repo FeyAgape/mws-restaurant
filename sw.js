89e3103e3d49c4d44bb0c69ec5e64c9751002023
TALENTWORKS\Fey.ijaware <fshola4real@gmail.com>
2018-08-07 16:45:55 +0100

    updated dbhelper.js to fetch the data from the udacity developement server, not from the local json

--- sw.js
+++ sw.js
@@ -1,32 +1,40 @@
-var staticCacheName = 'restaurant-v1.0';
-var staticImageCacheName = 'restaurant-imgs';
-var allCaches = [
-    staticCacheName,
-    staticImageCacheName
-];
-var urlsToCache = [
+const staticCacheName = 'restaurant-v1';
+
+const urlsToCache = [
     '/',
-    '/css/styles.css',
     '/index.html',
     '/restaurant.html',
     '/js/dbhelper.js',
     '/js/main.js',
-    '/js/restaurant_info.js'
+    '/js/restaurant_info.js',
+    '/img/'
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
+        caches.open(staticCacheName).then( cache => {
             return cache.addAll(urlsToCache);
-        })
-    );
+        }).catch(err => {
+        console.log(err);
+    }));
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+        cacheNames.filter( cacheName => {
+            return cacheName.startsWith('restaurant-') && cacheName !== staticCacheName;
+        }).map( cacheName => {
+            return cache.delete(cacheName);
+       });
+    }));
+});
+
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.open(staticCacheName).then(function (cache) {
-            return cache.match(event.request).then(function (response) {
-                return response || fetch(event.request).then(function (response) {
+        caches.open(staticCacheName).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request).then(response => {
                     cache.put(event.request, response.clone());
                     return response;
                 })
