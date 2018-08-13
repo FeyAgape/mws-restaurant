2a9502c4cc677bcbf677813f31b39cf4fcc388be
TALENTWORKS\Fey.ijaware <fshola4real@gmail.com>
2018-08-13 16:35:07 +0100

    fixed the put error in the sw.js file

--- sw.js
+++ sw.js
@@ -37,7 +37,6 @@ self.addEventListener('fetch', event => {
         caches.open(staticCacheName).then(cache => {
             return cache.match(event.request).then(response => {
                 return response || fetch(event.request).then(response => {
-                    cache.put(event.request, response.clone());
                     return response;
                 })
             })
